71b495ca3010eb3681a4be7295eb34db9571dacc
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 02:38:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,7 +35,7 @@ aoh_lookup([{:name=> "Don Gately", :occupation=> "Live-in Staff"},
        {:name=>  "Joelle van Dyne", :occupation=> "Radio Personality"},
        { :name=>  "Pat Monteseian", :occupation=> "Staff" },
        { :name=>  "Kate Gompert", :occupation=> "None" },
-       { :name=>  "Bruce Green", :occupation=> "Fan of Mildred" },2,:name)
+       { :name=>  "Bruce Green", :occupation=> "Fan of Mildred" },2, :name)
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
