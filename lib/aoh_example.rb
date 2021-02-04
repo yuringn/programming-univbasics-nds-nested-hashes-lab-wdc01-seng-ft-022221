26593b9fa1034b14991548cdcf964f75f4bd1b5f
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 02:42:50 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,19 +7,44 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
+  aoh = [{:name=> "Don Gately", :occupation=> "Live-in Staff"},
+         {:name=>  "Joelle van Dyne", :occupation=> "Radio Personality"},
+         { :name=>  "Pat Monteseian", :occupation=> "Staff" },
+         { :name=>  "Kate Gompert", :occupation=> "None" },
+         { :name=>  "Bruce Green", :occupation=> "Fan of Mildred" }
+  ]
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
 
 def literal_aoh
+  aoh = [{:name=> "Don Gately", :occupation=> "Live-in Staff"},
+         {:name=>  "Joelle van Dyne", :occupation=> "Radio Personality"},
+         { :name=>  "Pat Monteseian", :occupation=> "Staff" },
+         { :name=>  "Kate Gompert", :occupation=> "None" },
+         { :name=>  "Bruce Green", :occupation=> "Fan of Mildred" }
+  ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
 
 def aoh_lookup(aoh, row, key)
+  p aoh[row][key]
 end
+aoh_lookup([{:name=> "Don Gately", :occupation=> "Live-in Staff"},
+       {:name=>  "Joelle van Dyne", :occupation=> "Radio Personality"},
+       { :name=>  "Pat Monteseian", :occupation=> "Staff" },
+       { :name=>  "Kate Gompert", :occupation=> "None" },
+       { :name=>  "Bruce Green", :occupation=> "Fan of Mildred" }],2, :name)
 
 def aoh_update(aoh, row, key, new_value)
+  aoh[row][key]= new_value
+  p aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
+aoh_update([{:name=> "Don Gately", :occupation=> "Live-in Staff"},
+       {:name=>  "Joelle van Dyne", :occupation=> "Radio Personality"},
+       { :name=>  "Pat Monteseian", :occupation=> "Staff" },
+       { :name=>  "Kate Gompert", :occupation=> "None" },
+       { :name=>  "Bruce Green", :occupation=> "Fan of Mildred" }], 1, :name, "Joelle van Dyne (PGOAT)")
